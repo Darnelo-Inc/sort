a27256ac764b2cf9b57040b41debdf0a2d0e0567
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-11 20:44:29 +0300

    Revert "selectionSort added"
    This reverts commit 76aaca2dc6369d516af9da32ee2ca66bea827185.

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import { bubbleSort } from "./bubbleSort"
 import { quickSort } from "./quickSort"
-import { selectionSort } from "./selectionSort"
 
 const testArr: Number[] = [5, 2, 0, 0, -2, 100, -3]
 const expArr: Number[] = [-3, -2, 0, 0, 2, 5, 100]
 
 console.log("quickSort", quickSort(testArr))
 console.log("bubbleSort", bubbleSort(testArr))
-console.log("selectionSort", selectionSort(testArr))
 
 console.log("Expected", expArr)
