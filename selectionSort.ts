faf20e53008f58648e6fcb37029249bb63c52d26
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-11 20:45:47 +0300

    selectionSort added

---
+++ selectionSort.ts
@@ -0,0 +1,16 @@
+export function selectionSort(arr: Number[]): Number[] {
+  for (let i = 0; i < arr.length; i++) {
+    let min_ind = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min_ind]) min_ind = j
+    }
+
+    if (i !== min_ind) {
+      const tmp = arr[i]
+      arr[i] = arr[min_ind]
+      arr[min_ind] = tmp
+    }
+  }
+
+  return arr
+}
