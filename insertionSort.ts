5a6120b8e47313f28720ac4d0fe36c00eb4ffff0
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-20 09:37:51 +0300

    new naming in insertionSort

--- insertionSort.ts
+++ insertionSort.ts
@@ -1,14 +1,14 @@
 export function insertionSort(arr: Number[]): Number[] {
   for (let i = 1; i < arr.length; i++) {
-    const cur = arr[i]
     let sorted = i - 1
+    const current = arr[i]
 
-    while (sorted > -1 && cur < arr[sorted]) {
+    while (sorted > -1 && current < arr[sorted]) {
       arr[sorted + 1] = arr[sorted]
       sorted--
     }
 
-    arr[sorted + 1] = cur
+    arr[sorted + 1] = current
   }
 
   return arr
